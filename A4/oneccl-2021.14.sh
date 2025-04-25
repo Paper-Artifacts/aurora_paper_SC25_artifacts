665244ad8c0253434f260cc340c9d27a22d231fa
Nathan S. Nichols <8723338+nscottnichols@users.noreply.github.com>
2025-04-25 19:08:36 +0000

    update queue

--- A4/oneccl-2021.14.sh
+++ A4/oneccl-2021.14.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 mkdir -p /path/to/job/output
-qsub -l select=1    -l walltime=05:00:00 -l filesystems=flare -q lustre_scaling oneccl-2021.14.submit 
-qsub -l select=2    -l walltime=05:00:00 -l filesystems=flare -q lustre_scaling oneccl-2021.14.submit 
-qsub -l select=4    -l walltime=05:00:00 -l filesystems=flare -q lustre_scaling oneccl-2021.14.submit 
-qsub -l select=8    -l walltime=05:00:00 -l filesystems=flare -q lustre_scaling oneccl-2021.14.submit 
-qsub -l select=16   -l walltime=05:00:00 -l filesystems=flare -q lustre_scaling oneccl-2021.14.submit 
-qsub -l select=32   -l walltime=05:00:00 -l filesystems=flare -q lustre_scaling oneccl-2021.14.submit 
+qsub -l select=1    -l walltime=05:00:00 -l filesystems=flare -q prod           oneccl-2021.14.submit 
+qsub -l select=2    -l walltime=05:00:00 -l filesystems=flare -q prod           oneccl-2021.14.submit 
+qsub -l select=4    -l walltime=05:00:00 -l filesystems=flare -q prod           oneccl-2021.14.submit 
+qsub -l select=8    -l walltime=05:00:00 -l filesystems=flare -q prod           oneccl-2021.14.submit 
+qsub -l select=16   -l walltime=05:00:00 -l filesystems=flare -q prod           oneccl-2021.14.submit 
+qsub -l select=32   -l walltime=05:00:00 -l filesystems=flare -q prod           oneccl-2021.14.submit 
 qsub -l select=64   -l walltime=05:00:00 -l filesystems=flare -q prod           oneccl-2021.14.submit 
 qsub -l select=128  -l walltime=05:00:00 -l filesystems=flare -q prod           oneccl-2021.14.submit 
 qsub -l select=256  -l walltime=05:00:00 -l filesystems=flare -q prod           oneccl-2021.14.submit 
